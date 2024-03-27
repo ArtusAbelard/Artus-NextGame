05d9db28491983536328d9fc6065d887dd15fe0b
ArtusAbelard <tribelha@gmail.com>
2024-03-27 09:46:42 +0100

    start with redux instaled

---
+++ src/app/page.js
@@ -0,0 +1,29 @@
+"use client"
+
+import Image from "next/image";
+import { useDispatch ,useSelector } from "react-redux";
+import { increment,decrement } from "@/store/slice";
+
+export default function Home() {
+  const count = useSelector(state => state.counter.value)
+  const dispatch = useDispatch()
+  return (
+    <div>
+      
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      
+    </div>
+  );
+}
