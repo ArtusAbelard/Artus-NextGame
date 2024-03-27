14f7f6800c36dd79980dc5046e2b55a50ced51f5
ArtusAbelard <tribelha@gmail.com>
2024-03-27 12:28:27 +0100

    fetching data

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,23 @@
-
 import Btndecrement from "./ui/Btndecrement";
 
 async function getProduct(){
-  const res = await fetch('https://dummyjson.com/products?limit=12&select=title,price,images')
-  const data = await res.json()
-  return data.products
+  const res = await fetch('https://www.freetogame.com/api/games')
+  return res.json()
 }
 
 export default async function Home() {
   const products = await getProduct()
-
+  console.log(products);
   return (
     <div className="min-w-screen min-h-screen">
       <Btndecrement></Btndecrement>
+      {
+        products.map((Element,id)=>{
+          return(
+            <h1>{Element.title}</h1>
+          )
+        })
+      }
     </div>
   );
 }
