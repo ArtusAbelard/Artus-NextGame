ea3f522b0f593745c1a55d1ca4b729e507440d30
ArtusAbelard <tribelha@gmail.com>
2024-03-27 10:31:44 +0100

    add redux

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,19 @@ import { useDispatch,useSelector } from "react-redux";
 // import { useDispatch ,useSelector } from "react-redux";
 import { increment,decrement } from "@/store/slice";
 
+// async function getProduct(){
+//   const res = await fetch('https://dummyjson.com/products?limit=12&select=title,price,images')
+//   const data = await res.json()
+//   return data.products
+// }
+
+
 export default function Home() {
+  // const products = await getProduct()
   const count = useSelector(state => state.counter.value)
   const dispatch = useDispatch()
+
+  
   return (
     <div>
       
