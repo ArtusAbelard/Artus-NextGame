4a1da8b332e76bf34f5c5e7a2aabcc01dade8a31
ArtusAbelard <tribelha@gmail.com>
2024-03-27 21:17:51 +0100

    burger menu work

--- src/app/ui/Navbar.jsx
+++ src/app/ui/Navbar.jsx
@@ -14,9 +14,9 @@ export default function Navbar(props) {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 invert" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </div>
                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
-                    <li><a>Item 3</a></li>
+                    <li><Link href='/'>Home</Link></li>
+                    <li><Link href='/produits'>Produits</Link></li>
+                    <li><Link href='/favoris'>Favoris</Link></li>
                 </ul>
                 </div>
                 <a className="btn btn-ghost text-xl invert">GeekWorld</a>
