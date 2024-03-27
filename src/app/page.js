6a3581c42ca3bf040fad019612f6a834e86eee2e
ArtusAbelard <tribelha@gmail.com>
2024-03-27 11:24:50 +0100

    cleaning

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,18 @@
-"use client"
 
-import Image from "next/image";
-import { useDispatch,useSelector } from "react-redux";
-// import { useDispatch ,useSelector } from "react-redux";
-import { increment,decrement } from "@/store/slice";
+import Btndecrement from "./ui/Btndecrement";
 
-// async function getProduct(){
-//   const res = await fetch('https://dummyjson.com/products?limit=12&select=title,price,images')
-//   const data = await res.json()
-//   return data.products
-// }
+async function getProduct(){
+  const res = await fetch('https://dummyjson.com/products?limit=12&select=title,price,images')
+  const data = await res.json()
+  return data.products
+}
 
+export default async function Home() {
+  const products = await getProduct()
 
-export default function Home() {
-  // const products = await getProduct()
-  const count = useSelector(state => state.counter.value)
-  const dispatch = useDispatch()
-
-  
   return (
-    <div>
-      
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      
+    <div className="min-w-screen min-h-screen">
+      <Btndecrement></Btndecrement>
     </div>
   );
 }
---
+++ src/app/ui/Btndecrement.jsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React from 'react'
+import { useDispatch,useSelector } from "react-redux";
+import { increment,decrement } from "@/store/slice";
+
+export default function Btndecrement(props) {
+    const dispatch = useDispatch()
+    const count = useSelector(state => state.counter.value)
+    
+
+    return (
+    <div>
+      
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      
+    </div>
+    )
+}
