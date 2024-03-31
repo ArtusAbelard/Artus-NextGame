d8cf0624ed3501969077d6f95c2983eb71cf6e1c
ArtusAbelard <tribelha@gmail.com>
2024-03-31 23:17:18 +0200

    good size div home page

--- src/app/ui/Homepage.jsx
+++ src/app/ui/Homepage.jsx
@@ -5,7 +5,7 @@ export default function Homepage(props) {
 
     return (
         <div className='w-screen h-[55.9rem] flex flex-col justify-center items-center'>
-            <div className='w-screen h-[22rem]  flex items-center justify-center'>
+            <div className='w-screen h-[20rem] flex items-center justify-center'>
                 <div className='w-[103rem] h-[18rem] bg-slate-400 rounded-xl'>
 
                 </div>
