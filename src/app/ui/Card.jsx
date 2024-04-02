265cc9c06c5d7aa473548a94fd4bd91c1d3769ef
ArtusAbelard <tribelha@gmail.com>
2024-04-02 19:18:55 +0200

    add shear items

--- src/app/ui/Card.jsx
+++ src/app/ui/Card.jsx
@@ -12,12 +12,18 @@ export default function Card({products}) {
     dispatch(addToLib(products));
     const lib = useSelector(state => state.counter.lib)
    
-    
     return (
         <div className={`mt-[8rem] pt-[3rem]  flex bgnoborder bg-[#1e1e1e] justify-center items-center flex-wrap gap-10  ${color.actual==true?"invert":""}`}>
-            <div className="  h-[5rem] flex items-center w-[103rem]">
-                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            </div>
+            <div className="  h-[5rem]  text-white flex items-center w-[103.5rem] gap-[75rem]">
+                <input type="text" placeholder="Shear" className="w-96 input input-bordered  bg-black" />
+                <div className="dropdown dropdown-left">
+                <div tabIndex={0} role="button" className="btn m-1 bg-black text-white">Click</div>
+                <ul tabIndex={0} className="dropdown-content bg-black text-white z-[1] menu p-2 shadow rounded-box w-52">
+                    <li><a>Item 1</a></li>
+                    <li><a>Item 2</a></li>
+                </ul>
+                </div>
+                </div>
             {
                 products.map((Element,id)=>{
                     return(
