1d02e47c2829160d0af0c4ce5f20276fc3750ea1
ArtusAbelard <tribelha@gmail.com>
2024-04-02 18:09:19 +0200

    add input

--- src/app/ui/Card.jsx
+++ src/app/ui/Card.jsx
@@ -15,6 +15,9 @@ export default function Card({products}) {
     
     return (
         <div className={`mt-[8rem] pt-[3rem]  flex bgnoborder bg-[#1e1e1e] justify-center items-center flex-wrap gap-10  ${color.actual==true?"invert":""}`}>
+            <div className="  h-[5rem] flex items-center w-[103rem]">
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            </div>
             {
                 products.map((Element,id)=>{
                     return(
