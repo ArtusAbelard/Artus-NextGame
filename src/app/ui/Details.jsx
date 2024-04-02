6b9251b39686571601862a7de270952405e059a6
ArtusAbelard <tribelha@gmail.com>
2024-04-02 16:32:23 +0200

    add padding details

--- src/app/ui/Details.jsx
+++ src/app/ui/Details.jsx
@@ -7,7 +7,7 @@ export default function Details(props) {
     const color = useSelector((state) => state.color)
     
     return (
-        <div className={`pt-16 w-[100%] min-h-[895px] flex bg-[#1e1e1e] justify-center items-center flex-wrap gap-10 bgdetail max-[426px]:pt-0  ${color.actual==true?"invert":""}`}>
+        <div className={`mt-[6rem]  w-[100%] min-h-[895px] flex bg-[#1e1e1e] justify-center items-center flex-wrap gap-10 bgdetail max-[426px]:pt-0  ${color.actual==true?"invert":""}`}>
             <div className='w-[65rem] h-[40rem] rounded-3xl flex flex-col items-center justify-center bg-[#0E0E0E] boxshadow max-[1024px]:w-[50rem] max-[769px]:w-[40rem] max-[426px]:h-[40rem] max-[426px]:w-[20rem]  '>
                  <div className='w-full h-1/2  flex max-[426px]:flex-col'>
                     <div className='h-full w-1/2 flex items-center justify-center mt-[5rem] max-[426px]:w-full  max-[426px]:mb-[9rem] max-[426px]:mt-[3rem]'>
