bbdd50c5305c7e50faa0f83c8bff5070f1cefcf8
ArtusAbelard <tribelha@gmail.com>
2024-04-04 21:54:12 +0200

    all good

--- src/app/ui/Carddaisy.jsx
+++ src/app/ui/Carddaisy.jsx
@@ -22,7 +22,7 @@ export default function  Carddaisy({Element,id}) {
                             <h2 className="card-title">{Element.title}</h2>
                             <p>{Element.short_description}</p>  
                             {/*if ellement allready in basket hide star also funct to push in basket*/}
-                            {panier.panier.includes(Element.title)?"":<Image onClick={()=>{panier.panier.includes(Element.title)?"":dispatch(panierset(Element.title),dispatch(imgset(Element.thumbnail)),sethide("hidden"))}} className={`invert cursor-pointer ms-1 w-[20px] h-[20px] hover:w-[25px] hover:h-[25px] transition-[3s] ${hide}`}  alt="" src={star}></Image>}
+                            {connexion.status==false?"":<Image onClick={()=>{panier.panier.includes(Element.title)?"":dispatch(panierset(Element.title),dispatch(imgset(Element.thumbnail)),sethide("hidden"))}} className={`invert cursor-pointer ms-1 w-[20px] h-[20px] hover:w-[25px] hover:h-[25px] transition-[3s] ${hide}`}  alt="" src={star}></Image>}
                             <div className="card-actions justify-end">
                                 <Btndetail id={id} bails={Element}></Btndetail>
                             </div>
